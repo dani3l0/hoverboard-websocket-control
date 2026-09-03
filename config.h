e216adf8fbdd9d500eeaa2448db9bce43ecfc30e
dani3l0 <daniel.jank@protonmail.com>
2026-09-03 19:01:20 +0200

    sketch: RSSI info and emergency AP

--- config.h
+++ config.h
@@ -1,8 +1,11 @@
-#define WIFI_SSID           "mywifi"
-#define WIFI_PASSWORD       "mywifipass"
-#define RX_PIN              1
-#define TX_PIN              2
-#define SERIAL2_BAUD_RATE   115200
+#define WIFI_SSID             "mywifi"
+#define WIFI_PASSWORD         "mywifipass"
+#define EMERGENCY_AP_SSID     "Hoverboard32"
+#define EMERGENCY_AP_PASSWORD "12345678"
+#define EMERGENCY_AP_TIMEOUT  20
+#define RX_PIN                1
+#define TX_PIN                2
+#define SERIAL2_BAUD_RATE     115200
 
 #if __has_include("config-custom.h")
   #include "config-custom.h"
--- hoverboard-websocket-control.ino
+++ hoverboard-websocket-control.ino
@@ -6,6 +6,8 @@
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
+AsyncWebSocket wsRssi("/rssi");
+int connectedClients = 0;
 
 // Handle websocket incoming messages
 void handleWebSocketMessage(void *arg, uint8_t *data, size_t len) {
@@ -22,11 +24,13 @@ void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType 
     // On new client connection
     case WS_EVT_CONNECT:
       Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString());
+      connectedClients++;
       break;
 
     // On client disconnected
     case WS_EVT_DISCONNECT:
       Serial.printf("WebSocket client #%u disconnected\n", client->id());
+      connectedClients--;
       break;
 
     // On new incoming data
@@ -47,24 +51,46 @@ void setup(){
   LittleFS.begin(true);
 
   // Connect to Wi-Fi
+  WiFi.setAutoReconnect(true);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to WiFi");
-  while (WiFi.status() != WL_CONNECTED) {
+  Serial.printf("Connecting to WiFi");
+  int attempts = 0;
+  while (WiFi.status() != WL_CONNECTED && attempts < EMERGENCY_AP_TIMEOUT) {
     delay(1000);
-    Serial.print(".");
+    Serial.printf(".");
+    attempts++;
   }
-  Serial.printf("\nDevice IP: %s\n", WiFi.localIP().toString());
+  if (WiFi.status() == WL_CONNECTED) Serial.printf("\nDevice IP: %s\n", WiFi.localIP().toString());
+  else {
+    Serial.printf("\nFailed to connect to '%s'. Hosting an emergency AP...\n", WIFI_SSID);
+    WiFi.disconnect();
+    WiFi.setAutoReconnect(false);
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(EMERGENCY_AP_SSID, EMERGENCY_AP_PASSWORD);
+    Serial.printf("Hotspot active! Connect to '%s' with password '%s'\n", EMERGENCY_AP_SSID, EMERGENCY_AP_PASSWORD);
+    Serial.printf("Hostpot IP: %s\n", WiFi.softAPIP().toString());
+  }
 
   // Initialize websocket server
   ws.onEvent(onEvent);
   server.addHandler(&ws);
+  server.addHandler(&wsRssi);
   server.serveStatic("/", LittleFS, "/html/").setDefaultFile("index.html");
   server.begin();
 }
 
+int websocketSendCounter = 0;
+
 void loop() {
   // Cleanup obsolete websocket clients
   ws.cleanupClients();
+  wsRssi.cleanupClients();
+  
+  // Send RSSI information
+  if (websocketSendCounter++ > 100) {
+    if (WiFi.status() == WL_CONNECTED) wsRssi.textAll(String(WiFi.RSSI()) + "," + String(connectedClients));
+    websocketSendCounter = 0;
+  }
 
   // Read Serial2 data and send it via websockets
   if (Serial2.available()) {
